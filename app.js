b650690edee52da695ffd6200b30d64260e2d4cd
cuoigl <cuoi.dev@gmail.com>
2023-03-20 11:22:51 +0700

    asd

--- app.js
+++ app.js
@@ -45,11 +45,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/tutorials", tutorialsRouter);
 app.use("/imageUpload", uploadRouter);
 
